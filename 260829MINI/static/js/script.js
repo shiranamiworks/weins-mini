864b02f767d5a25c5747fdd424cf06542f04ee0d
橋本真奈美 <manako.075h@gmail.com>
2026-08-20 13:01:36 +0900

    no message

--- 260829MINI/static/js/script.js
+++ 260829MINI/static/js/script.js
@@ -20,23 +20,37 @@ $(function(){
   }
 });
 
+//$(function(){
+//$('.main-visual .line03')
+//    .animate({'opacity':'1'}, 600)
+//    .animate({'height':'52%'}, 700);
+//        
+//$('.main-visual .line04')
+//    .animate({'opacity':'1'}, 600)
+//    .animate({'width':'54%'}, 700);
+//        
+//$('.main-visual .line05')
+//    .animate({'opacity':'1'}, 600)
+//    .animate({'width':'10%'}, 700);
+//        
+//$('.main-visual .mv01')
+//    .animate({'opacity':'1'}, 800)
+//    .animate({'left':'3%'}, 600)
+//    
+//});
+
 $(function(){
-$('.main-visual .line03')
-    .animate({'opacity':'1'}, 600)
-    .animate({'height':'52%'}, 700);
-        
-$('.main-visual .line04')
-    .animate({'opacity':'1'}, 600)
-    .animate({'width':'54%'}, 700);
-        
-$('.main-visual .line05')
-    .animate({'opacity':'1'}, 600)
-    .animate({'width':'10%'}, 700);
-        
-$('.main-visual .mv01')
-    .animate({'opacity':'1'}, 800)
-    .animate({'left':'3%'}, 600)
-    
+  $('.roop-img').slick({
+    autoplay: true, // 自動でスクロール
+    autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+    speed: 20000, // スライドが流れる速度を設定
+    cssEase: "linear", // スライドの流れ方を等速に設定
+    slidesToShow: .4, // 表示するスライドの数
+    swipe: false, // 操作による切り替えはさせない
+    arrows: false, // 矢印非表示
+    pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+  });
 });
 
 //$(function(){
